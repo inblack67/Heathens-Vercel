774e11adc91e4f482d0de3ebec0375bd0a17d2c9
Aman Bhardwaj <inblack>
2021-01-03 23:45:38 +0530

    bug fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,3 @@
 export const routes = {
-    privateRoutes: [ '/dashboard' ]
+    privateRoutes: [ '/dashboard', '/channels' ]
 }
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,7 @@
-import * as contextTypes from '../context/contextTypes';
 import { ISeverities } from './interfaces';
-
-const myContextTypes = Object.values( contextTypes );
 
 export type SState = {
     message: string;
     isActive: boolean;
     severity: ISeverities;
-};
-
-export type SAction = {
-    payload: SState,
-    type: typeof myContextTypes[ number ];
 };
